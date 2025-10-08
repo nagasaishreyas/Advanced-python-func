ffacc77e04610cde689e3b6bc07cfb147d043bc7
sudheerkuntumalla <sudheerkuntumalla3004@gmail.com>
2025-10-08 20:47:35 +0530

    guess

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,41 +1,52 @@
-'''Hi this is TIC TAC TOE you can control by using up, dowm, right, left arrows.'''
-theboard = {'7': '', '8': '', '9': '',
-            '4': '', '5': '', '6': '',
-            '1': '', '2': '', '3': ''}
+#Implementation of Two Player Tic-Tac-Toe game in Python.
 
-board_key = []
+''' We will make the board using dictionary 
+    in which keys will be the location(i.e : top-left,mid-right,etc.)
+    and initialliy it's values will be empty space and then after every move 
+    we will change the value according to player's choice of move. '''
 
-for key in theboard:
-    board_key.append(key)
-    
-'''Updated board will be updated.'''
-    
+theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
+            '4': ' ' , '5': ' ' , '6': ' ' ,
+            '1': ' ' , '2': ' ' , '3': ' ' }
+
+board_keys = []
+
+for key in theBoard:
+    board_keys.append(key)
+
+''' We will have to print the updated board after every move in the game and 
+    thus we will make a function in which we'll define the printBoard function
+    so that we can easily print the board everytime by calling this function. '''
+
 def printBoard(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
     print('-+-+-')
     print(board['4'] + '|' + board['5'] + '|' + board['6'])
     print('-+-+-')
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
-    
-def game():   
- turn = 'X'
- count = 0
- 
- 
- for i in range(10):
-      printboard(theboard)
-      print("Its your turn", + turn, + "switch to move")
-      move = input()
-      
-      if theBoard[move] == ' ':
+
+# Now we'll write the main function which has all the gameplay functionality.
+def game():
+
+    turn = 'X'
+    count = 0
+
+
+    for i in range(10):
+        printBoard(theBoard)
+        print("It's your turn," + turn + ".Move to which place?")
+
+        move = input()        
+
+        if theBoard[move] == ' ':
             theBoard[move] = turn
             count += 1
-      else:
+        else:
             print("That place is already filled.\nMove to which place?")
             continue
 
         # Now we will check if player X or O has won,for every move after 5 moves. 
-      if count >= 5:
+        if count >= 5:
             if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ': # across the top
                 printBoard(theBoard)
                 print("\nGame Over.\n")                
@@ -78,19 +89,19 @@ def game():
                 break 
 
         # If neither X nor O wins and the board is full, we'll declare the result as 'tie'.
-      if count == 9:
+        if count == 9:
             print("\nGame Over.\n")                
             print("It's a Tie!!")
 
         # Now we have to change the player after every move.
-      if turn =='X':
+        if turn =='X':
             turn = 'O'
-      else:
+        else:
             turn = 'X'        
     
     # Now we will ask if player wants to restart the game or not.
- restart = input("Do want to play Again?(y/n)")
- if restart == "y" or restart == "Y":  
+    restart = input("Do want to play Again?(y/n)")
+    if restart == "y" or restart == "Y":  
         for key in board_keys:
             theBoard[key] = " "
 
